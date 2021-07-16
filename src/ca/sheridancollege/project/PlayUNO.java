607ab676b4a6e47e373c2e6234c25662dac4a549
Nadeem <rashinad@sheridancollege.ca>
2021-07-16 15:03:25 -0400

    Added PlayUNO Main Class

---
+++ src/ca/sheridancollege/project/PlayUNO.java
@@ -0,0 +1,17 @@
+package ca.sheridancollege.project;
+
+/**
+ * This class +++Insert Description Here+++
+ *
+ * @author Nadeem Rashid
+ */
+public class PlayUNO {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        
+    }
+
+}
