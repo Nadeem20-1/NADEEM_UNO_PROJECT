a1b1bbe7a7f5faa09abb478a0062b53c821aadae
Nadeem <rashinad@sheridancollege.ca>
2021-07-16 18:57:27 -0400

    Added Final Variables for Colours and Number Added number and colour variables Added encapsulation

--- src/ca/sheridancollege/project/Card.java
+++ src/ca/sheridancollege/project/Card.java
@@ -14,7 +14,11 @@ package ca.sheridancollege.project;
  */
 public abstract class Card {
     //default modifier for child classes
-
+    private static final int[] cardNumbers = {0,1,2,3,4,5,6,7,8,9};
+    private static final String[] cardColours = {"Red","Yellow","Green","Blue"};
+    
+    private int number;
+    private String colour;
     /**
      * Students should implement this method for their specific children classes
      *
@@ -22,5 +26,47 @@ public abstract class Card {
      */
     @Override
     public abstract String toString();
+    
+    public Card(int number, String colour){
+        this.number = number;
+        this.colour = colour;
+    }
+    
+    public String[] getCardColours(){
+        return cardColours;
+    }
+    
+    public int[] getCardNumbers(){
+        return cardNumbers;
+    }
+    
+    /**
+     * @return the number
+     */
+    public int getNumber() {
+        return number;
+    }
+
+    /**
+     * @param number the number to set
+     */
+    public void setNumber(int number) {
+        this.number = number;
+    }
+
+    /**
+     * @return the colour
+     */
+    
+    public String getColour() {
+        return colour;
+    }
+
+    /**
+     * @param colour the colour to set
+     */
+    public void setColour(String colour) {
+        this.colour = colour;
+    }
 
 }
