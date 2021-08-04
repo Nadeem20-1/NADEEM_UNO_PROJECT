8be16ba0f749e64d407959402832b5f1c0b28d42
Nadeem <rashinad@sheridancollege.ca>
2021-08-04 16:27:59 -0400

    Added new Play class Added ability to start game in PlayUNO

---
+++ src/ca/sheridancollege/project/Play.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package ca.sheridancollege.project;
+
+/**
+ *
+ * @author ukule
+ */
+public class Play extends Game{
+
+    @Override
+    public void play() {
+        
+    }
+
+    @Override
+    public void declareWinner() {
+        
+    }
+    
+}
--- src/ca/sheridancollege/project/PlayUNO.java
+++ src/ca/sheridancollege/project/PlayUNO.java
@@ -1,30 +1,38 @@
 package ca.sheridancollege.project;
 
+import java.util.Scanner;
+
 /**
  * This class +++Insert Description Here+++
  *
  * @author Nadeem Rashid
  */
-public class PlayUNO extends Game{
+
+public class PlayUNO{
 
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) {
         boolean playAgain = true;
+        String input;
+        Scanner s = new Scanner(System.in);
         
-        while(playAgain = true){
-        
+        while(playAgain == true){
+            System.out.println("Welcome to Nadeem's UNO game!");
+            System.out.println("To play, please type 'play'!");
+            input = s.next();
+            if(input.equalsIgnoreCase("play")){
+                Play game = new Play();
+                game.play();
+                System.out.println("Thanks for playing!");
+                while(!input.equalsIgnoreCase("again")){
+                    System.out.println("To play again, type 'again'!");
+                    System.out.println("To stop playing, type 'stop'!");
+                    if(input.equalsIgnoreCase("stop"))
+                        playAgain = false;
+                }
+            }
         }
-    }
-
-    @Override
-    public void play() {
-        
-    }
-
-    @Override
-    public void declareWinner() {
-        
     }
 }
