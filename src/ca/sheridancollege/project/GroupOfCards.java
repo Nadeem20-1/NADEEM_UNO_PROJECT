e73e41b54523920281d05033b619bd966f2a5338
Nadeem <rashinad@sheridancollege.ca>
2021-08-11 00:21:30 -0400

    Added addCard and removeCard methods Changed drawCard to be more efficient

--- src/ca/sheridancollege/project/GroupOfCards.java
+++ src/ca/sheridancollege/project/GroupOfCards.java
@@ -1,7 +1,7 @@
-/**
- * SYST 17796 Project Base code.
- * Students can modify and extend to implement their game.
- * Add your name as an author and the date!
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
  */
 package ca.sheridancollege.project;
 
@@ -9,19 +9,19 @@ import java.util.ArrayList;
 import java.util.Collections;
 
 /**
- * A concrete class that represents any grouping of cards for a Game. HINT, you might want to subclass this more than
- * once. The group of cards has a maximum size attribute which is flexible for reuse.
  *
- * @author dancye
- * @author Paul Bonenfant Jan 2020
- * @author Megha Patel
+ * @author Nadeem Rashid
  */
 public class GroupOfCards extends Card{
 
     //The group of cards, stored in an ArrayList
     private ArrayList<Card> cards = new ArrayList();
     private int size;//the size of the grouping
-
+    
+    public GroupOfCards(){
+        
+    }
+    
     public GroupOfCards(int size) {
         this.size = size;
     }
@@ -34,26 +34,43 @@ public class GroupOfCards extends Card{
     public ArrayList<Card> getCards() {
         return cards;
     }
-    
-    public void drawCard(){
-        int num = (int)Math.round(Math.random()*9);
-        int colour = (int)Math.round(Math.random()*3);
-        int special = (int)Math.round(Math.random()*2);
-        int chance = (int)Math.round(Math.random()*10+1);
-        String[] cardColours = getCardColours();
-        String[] specialCard = getSpecialCards();
-        if(chance == 1){
-            Card card = new UNOCard(num,cardColours[colour],specialCard[special]);
-            cards.add(card);
-        }else{
-            Card card = new UNOCard(num,cardColours[colour],specialCard[3]);
-            cards.add(card);
-        }
+    //This method will add the card to the ArrayList storing these cards
+    public void addCard(Card card){
+        cards.add(card);
         size++;
     }
-    
+    //This method will remove the card to the ArrayList storing these cards
+    public void removeCard(Card card){
+        cards.remove(card);
+        size--;
+    }
+    //This method will be used to draw a random card
+    public void drawCard(){
+        //A random number will be drawn
+        int num = (int)Math.round(Math.random()*8);
+        //A random colour will be picked
+        int colour = (int)Math.round(Math.random()*3);
+        //A random special varaible will be chosen
+        int special = (int)Math.round(Math.random()*2);
+        //There is also a luck factor involved with getting the special card.
+        int chance = (int)Math.round(Math.random()*9)+1;
+        int[] cardNumbers = getCardNumbers();
+        String[] cardColours = getCardColours();
+        String[] specialCard = getSpecialCards();
+        //If the drawn card's random lucky number is a 1, then it will generate
+        //a random special card
+        if(chance == 1){
+            Card card = new UNOCard(cardColours[colour],specialCard[special]);
+            addCard(card);
+        }else{
+            //Otherwise, it will draw a normal card
+            Card card = new UNOCard(cardNumbers[num],cardColours[colour],specialCard[3]);
+            addCard(card);
+        }
+    }
+    //For privacy reasons, this method will be used to hide the previous person's hand
     public void hidePrevHand(){
-        for(int i = 0;i<15;i++){
+        for(int i = 0;i<20;i++){
             System.out.println();
         }
     }
