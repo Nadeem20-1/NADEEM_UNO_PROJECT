96de849b27eb895f9757374fe964f9ec5d49f9e1
Nadeem <rashinad@sheridancollege.ca>
2021-08-11 00:24:13 -0400

    Added new constructor for special cards Changed toString method to take into account regular and special cards

--- src/ca/sheridancollege/project/UNOCard.java
+++ src/ca/sheridancollege/project/UNOCard.java
@@ -7,15 +7,26 @@ package ca.sheridancollege.project;
 
 /**
  *
- * @author ukule
+ * @author Nadeem Rashid
  */
 public class UNOCard extends Card{
+    //This method will be used to create a special card
+    public UNOCard(String colour, String special){
+        super(colour, special);
+    }
+    //This method will be used to create a regular card
     public UNOCard(int number, String colour, String special){
         super(number, colour, special);
     }
-
+    
+    //If the card contains a number, it is specified as a regular card,
+    //as a special card does not contain numbers.
     @Override
     public String toString() {
-        return "";
+        String[] specialCard = getSpecialCards();
+        if(getSpecial().equals(specialCard[3])){
+            return getColour() + " " + getNumber();
+        }else
+        return getColour() + " " + getSpecial();
     }
 }
