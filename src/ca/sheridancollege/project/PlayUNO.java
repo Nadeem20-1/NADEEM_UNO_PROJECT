7bb5dc9743ea36ce22323753bfb881a84d4dc5ba
Nadeem <rashinad@sheridancollege.ca>
2021-08-11 00:22:48 -0400

    Added loop, and finished rest of code to initiate new game

--- src/ca/sheridancollege/project/PlayUNO.java
+++ src/ca/sheridancollege/project/PlayUNO.java
@@ -1,13 +1,16 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
 package ca.sheridancollege.project;
 
 import java.util.Scanner;
 
 /**
- * This class +++Insert Description Here+++
  *
  * @author Nadeem Rashid
  */
-
 public class PlayUNO{
 
     /**
@@ -16,21 +19,29 @@ public class PlayUNO{
     public static void main(String[] args) {
         boolean playAgain = true;
         String input;
+        String gameName = "Nadeem's UNO Game";
         Scanner s = new Scanner(System.in);
         
+        //While loop allows users to play infinitely, until they want to quit
         while(playAgain == true){
-            System.out.println("Welcome to Nadeem's UNO game!");
+            System.out.println("Welcome to " + gameName + "!");
             System.out.println("To play, please type 'play'!");
             input = s.next();
             if(input.equalsIgnoreCase("play")){
+                //Creates a new instance of 'Play', which allows users to play
+                //the game
                 Play game = new Play();
                 game.play();
-                System.out.println("Thanks for playing!");
+                //Once game is finished, the rest of this code will run
+                System.out.println("\nThanks for playing!");
                 while(!input.equalsIgnoreCase("again")){
                     System.out.println("To play again, type 'again'!");
                     System.out.println("To stop playing, type 'stop'!");
-                    if(input.equalsIgnoreCase("stop"))
+                    input = s.next();
+                    if(input.equalsIgnoreCase("stop")){
                         playAgain = false;
+                        break;
+                    }
                 }
             }
         }
